a08e8391e33e3c74e781836844fc1d981856e295
Hash2O <archeosam@gmail.com>
2025-08-31 11:13:14 +0200

    Creation of scen flow : start to main, main to start, exit (Editor and Built Mode)

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+// This namespace will only be included when compiling within the Unity Editor
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
@@ -20,5 +25,20 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Conditionnal compiling
+    public void Exit()
+    {
+    #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+    #else
+        Application.Quit(); // Only works in the built application, not while testing in Editor mode
+    #endif
     }
 }
